309b1110a31d2d17a8d23901bf97c79cfad3b2f1
Dipkumar Patel <dippatel1994@gmail.com>
2022-03-26 00:39:33 -0400

    Fixed pie chart error

--- FrontEnd/src/components/Chart.js
+++ FrontEnd/src/components/Chart.js
@@ -1,5 +1,8 @@
-import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
+import React, { Component } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export class Chart extends Component {
   constructor(props) {
@@ -20,53 +23,35 @@ export class Chart extends Component {
     return (
       <div className="chart">
         CHART
-        {/* <Bar
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        /> */}
         <Pie
-          data={{
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [
-              {
-                label: "# of votes",
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(255, 206, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-              },
-              // {
-              //   label: 'Quantity',
-              //   data: [47, 52, 67, 58, 9, 50],
-              //   backgroundColor: 'orange',
-              //   borderColor: 'red',
-              // },
-            ],
-          }}
+          data={
+            {
+              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+              datasets: [
+                {
+                  label: '# of Votes',
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }
+          }
           height={400}
           width={600}
           options={{
@@ -87,102 +72,9 @@ export class Chart extends Component {
             },
           }}
         />
-        {/* <Line
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        />
-        <Pie
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        /> */}
       </div>
     );
   }
 }
 
 export default Chart;
-
-// import React from "react";
-// import { Pie } from "react-chartjs-2";
-
-// const BarChart = () => {
-//   return (
-//     <div>
-//       <Pie
-//         data={{
-//           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//           datasets: [
-//             {
-//               label: "# of votes",
-//               data: [12, 19, 3, 5, 2, 3],
-//               backgroundColor: [
-//                 "rgba(255, 99, 132, 0.2)",
-//                 "rgba(54, 162, 235, 0.2)",
-//                 "rgba(255, 206, 86, 0.2)",
-//                 "rgba(75, 192, 192, 0.2)",
-//                 "rgba(153, 102, 255, 0.2)",
-//                 "rgba(255, 159, 64, 0.2)",
-//               ],
-//               borderColor: [
-//                 "rgba(255, 99, 132, 1)",
-//                 "rgba(54, 162, 235, 1)",
-//                 "rgba(255, 206, 86, 1)",
-//                 "rgba(75, 192, 192, 1)",
-//                 "rgba(153, 102, 255, 1)",
-//                 "rgba(255, 159, 64, 1)",
-//               ],
-//               borderWidth: 1,
-//             },
-//             // {
-//             //   label: 'Quantity',
-//             //   data: [47, 52, 67, 58, 9, 50],
-//             //   backgroundColor: 'orange',
-//             //   borderColor: 'red',
-//             // },
-//           ],
-//         }}
-//         height={400}
-//         width={600}
-//         options={{
-//           maintainAspectRatio: false,
-//           scales: {
-//             yAxes: [
-//               {
-//                 ticks: {
-//                   beginAtZero: true,
-//                 },
-//               },
-//             ],
-//           },
-//           legend: {
-//             labels: {
-//               fontSize: 25,
-//             },
-//           },
-//         }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default BarChart;
