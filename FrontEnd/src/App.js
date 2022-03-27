a5c1c930101f2c30be43c7b536921eced56d8d3d
evansmosomi <evans.mosomi@ontariotechu.net>
2022-03-27 19:38:29 -0400

    Third commit

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 import "./index.css";
-import Chart from "./components/Chart";
+import Chartgraph from "./components/Chartgraph";
 //import StudentForm from "./components/StudentForm";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      chartData: {},
-    };
-  }
-
-  componentWillMount() {
-    // this.getchartData(); // this should be this.getChartData();
-    this.getChartData();
-  }
-
-  
-
   render() {
     return (
       <div>
-        <Chart />
+        <Chartgraph />
       </div>
-    ); 
+    );
   }
 }
 
